f3c78d1e67577e69c04e6a059acd72862bad74df
Steve Duitsman <steve.duitsman@omniresources.com>
2017-01-10 09:31:26 -0600

    finish select input add author actions add author reducer
    finish select input
    add author actions
    add author reducer

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -6,23 +6,33 @@ const SelectInput = ({name, label, onChange, defaultOption, value, error, option
       <label htmlFor={name}>{label}</label>
       <div className="field">
         {
-          <select
-            name={name}
-            value={value}
-            onChange={onChange}
-            className="form-control">
+          <select name={name}
+                  value={value}
+                  onChange={onChange}
+                  className="form-control">
             <option value="">{defaultOption}</option>
             {
               options.map(option => {
-                return <option key={option.value} value={option.value}>{option.text}</option>;
+                return <option key={option.value} 
+                              value={option.value}>{option.text}</option>;
               })
             }
-          }
           </select>
-          {error && <div className="alert alert-danger">{error}</div>}
         }
+        {error && <div className="alert alert-danger">{error}</div>}
       </div>
     </div>
   );
 };
 
+SelectInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  defaultOption: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default SelectInput;
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,25 +2,52 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions'
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
-
+      <CourseForm course={this.state.course}
+                  errors={this.state.errors}
+                  allAuthors={this.props.authors}/>
     );
   }
 }
 
-ManageCoursePage.PropTypes = {
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => {
+  let course = {
+    id: '',
+    watchHref: '',
+    title: '',
+    authorId: '',
+    length: '',
+    category: ''
+  };
+
+  const authorsFormattedForDropDown = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: `${author.firstName} ${author.lastName}`
+    };
+  });
+
   return {
-    state: state
+    course: course,
+    authors: authorsFormattedForDropDown
   };
 };
 
