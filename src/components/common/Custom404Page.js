4f6ddbeb326a9cdfbba4fd0041a2d12590a4c51f
Steve Duitsman <steve.duitsman@omniresources.com>
2017-01-12 10:48:01 -0600

    Add custom 404 page

---
+++ src/components/common/Custom404Page.js
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react';
+
+export const Custom404Page = () =>  {
+  return (
+    <div>
+      <h1>Oops</h1>
+      <p>The page you are looking for was not found.</p>
+    </div>
+  );
+};
+
+export default Custom404Page;
